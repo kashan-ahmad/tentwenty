0273fc1fc45ac064219a2bee4e47393cf7f865d4
Kashan Ahmad <kashan.aech.620@gmail.com>
2023-02-14 13:14:27 +0500

    feat: initialization

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+function App() {
+  return (
+    <div className="h-screen bg-slate-900 grid place-items-center">
+      <h1 className="text-white text-8xl">Digital Agency</h1>
+    </div>
+  )
+}
+
+export default App

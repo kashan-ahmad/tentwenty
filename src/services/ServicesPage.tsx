80fbf86af7efa48ca5f4c6d9ba610d78946ddefc
kashan-ahmad <kashanahmed@hwryk.com>
2023-02-28 13:03:56 +0500

    fix: added a little fix

--- src/services/ServicesPage.tsx
+++ src/services/ServicesPage.tsx
@@ -37,11 +37,39 @@ export default function ServicesPage({}: Props) {
     }
   }, []);
 
+  // Effect: Make the list scroll horizontally when vertically scrolled.
+  React.useEffect(() => {
+    const serviceList = document.getElementById("ServiceList");
+
+    if (serviceList) {
+      let scrolledHeight = 0;
+
+      serviceList.addEventListener("wheel", (e) => {
+        scrolledHeight += e.deltaY;
+
+        if (scrolledHeight < 0) scrolledHeight = 0;
+
+        if (scrolledHeight > serviceList.scrollWidth)
+          scrolledHeight = serviceList.scrollWidth;
+
+        console.log({ scrolledHeight });
+
+        serviceList.scroll({
+          left: scrolledHeight,
+          behavior: "smooth",
+        });
+      });
+    }
+  }, []);
+
   if (!services) return <Navigate to="/" />;
 
   return (
     <AnimatedLayout>
-      <ul className="overflow-auto max-h-screen flex flex-col md:flex-row md:overflow-y-hidden">
+      <ul
+        id="ServiceList"
+        className="overflow-auto max-h-screen flex flex-col md:flex-row md:overflow-y-hidden"
+      >
         {services.map((service, i) => (
           <li key={i}>
             <ServiceComponent index={i + 1} {...service} />
